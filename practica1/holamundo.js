3362c153bfcdc0d90a0e3309b94f178e426a2156
Dai <123047287@upq.edu.mx>
2026-05-12 15:09:23 -0600

    Finalización de la práctica 1

---
+++ practica1/holamundo.js
@@ -0,0 +1,28 @@
+/* Js del lado del servidor */
+
+console.log("Hola Mundo JS con Node")
+
+/* claculo */
+
+let edad1 = 12
+let edad2 = 34
+
+console.log("Edad promedio: ")
+console.log((edad1 + edad2)/2)
+
+/* medir el tiempo de ejecucion */
+
+console.time("miProceso")
+for(let i = 0; i < 10000000; i++){
+
+}
+console.timeEnd("miProceso")
+
+/* Objetos tipo tabla en la consola */
+
+let usuarios = [
+    {nombre:"Dai", edad:"21"},
+    {nombre:"Valeria", edad:"21"},
+
+];
+console.table(usuarios)
