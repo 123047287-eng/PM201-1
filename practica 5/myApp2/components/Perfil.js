15545eb46377412231baf2854e7ab644630e6ed1
Dai <123047287@upq.edu.mx>
2026-06-09 12:59:21 -0600

    Componente Perfil

---
+++ practica 5/myApp2/components/Perfil.js
@@ -0,0 +1,10 @@
+import {Text,View} from 'react-native'
+export const Perfil = () => {
+    <view>
+        <Text>Daira Valeria Curiel Morales </Text>
+        <Text> </Text>
+        <Text>Daira Valeria Curiel Morales </Text>
+        <Text>Daira Valeria Curiel Morales </Text>
+    </view>
+
+}
