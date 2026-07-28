778f9f4d444c5c976ecf0d0807340ebe8dfb9d34
Dai <123047287@upq.edu.mx>
2026-07-28 00:44:42 -0600

    Practica 20 Conexión Movil API Select & Insert

--- practica 5/myApp2/Screens/Componente1.js
+++ practica 5/myApp2/Screens/Componente1.js
@@ -1,30 +1,75 @@
-/* zona 1: lugar donde estan las importaiones de archivos y componentes */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+Componente1.js
 
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { MiModal} from '../components/MiModa';
+import { BottomSheet } from '../components/BotonSheet';
 
+export default function Componente1() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [sheetVisible, setSheetVisible] = useState(false);
 
-/* 
-zona 2: Main - Componentes del screen */
-
-export default function App() {
   return (
-    <View>
-        <Text>Aquí va la primer práctica de componentes nativos</Text>
-    </View>
+    <View style={styles.container}>
+      <Text style= {styles.titulo}>Practica : Modal y Bottom Sheet </Text>
 
+      <Pressable
+        style={[styles.boton, { backgroundColor: '#2a7e01' }]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.botonTexto}>Mostrar Modal</Text>
+      </Pressable>
+
+      <Pressable
+        style={[styles.boton, { backgroundColor: 'red' }]}
+        onPress={() => setSheetVisible(true)}
+      >
+        <Text style={styles.botonTexto}>Abrir Bottom Sheet</Text>
+      </Pressable>
+
+      <MiModal
+        visible={modalVisible}
+        onCerrar={() => setModalVisible(false)}
+        titulo="Modal"
+      >
+        <Text>Nombre: Saul</Text>
+        <Text>Carrera: Ing Sistemas</Text>
+        <Text>Cuatrimestre: 9 </Text>
+      </MiModal>
+
+      <BottomSheet
+        visible={sheetVisible}
+        onCerrar={() => setSheetVisible(false)}
+        titulo="Bottom Sheet"
+      >
+        <Text>Este sale desde abajo</Text>
+        <Text>Se puede cerrar tocando el area oscura</Text>
+      </BottomSheet>
+    </View>
+    
   );
 }
 
-
-/* zona 3: Estelizacion de los componentes [estilos] y posicionamiento*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection:'row',
-    justifyContent:'space-around' 
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  boton: {
+    paddingVertical: 12,
+    paddingHorizontal: 28,
+    borderRadius: 8,
+  },
+  botonTexto: {
+    color: 'white',
+    fontWeight: 'semibold',
+    fontSize: 16,
   },
 });
